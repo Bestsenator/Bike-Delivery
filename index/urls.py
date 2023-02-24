d0068fea24474bf95133e0c2bcaba6e23dde07b5
Bestsenator <senator136019@gmail.com>
2023-02-24 23:55:25 +0330

    edit index app - v2

--- index/urls.py
+++ index/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index-main'),
+    path('logout/', views.logout, name='logout'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index/index.html')
+    context = {
+        'IndexMain': 'active',
+    }
+    return render(request, 'index/index.html', context)
+
+
+def logout(request):
+    redirectUrl = reverse('login-main')
+    response = HttpResponseRedirect(redirectUrl)
+    response.delete_cookie('Session')
+    return response
