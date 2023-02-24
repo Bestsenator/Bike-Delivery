20f91d16f321a8caa2b073e43c587caf7a2aabbd
Bestsenator <senator136019@gmail.com>
2023-02-24 23:55:15 +0330

    edit login app - v2

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='login-main'),
+    path('check/', views.check, name='login-check'),
+    path('forgotPassword/', views.forgotPassword, name='login-forgotPassword'),
 
 ]
--- login/views.py
+++ login/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from funcs import auth
+from signup.models import Customer
+from funcs.cookies import set_cookie
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'login/index.html')
+    context = {}
+    if request.COOKIES.get('Notif401'):  # from portal --> session not found or expired
+        context = {
+            'SessionExpired': True,
+        }
+    return render(request, 'login/index.html', context)
+
+
+def check(request):
+    resAuth = auth.checkSession(request)
+    if resAuth.get('Status') == 200:
+        redirectUrl = reverse('portal-main')
+        return HttpResponseRedirect(redirectUrl)
+    if not request.POST:
+        context = {
+            'Status': 401  # change html file with inspect
+        }
+        return render(request, 'login/index.html', context)
+    email = request.POST.get('Email')
+    password = request.POST.get('Password')
+    rememberMe = request.POST.get('RememberMe')
+    listInput = [email, password]
+    for i in listInput:
+        if not i:
+            context = {
+                'Status': 403  # input incomplete
+            }
+            return render(request, 'login/index.html', context)
+    customerInfo = Customer.objects.filter(Email=email,Password=password).first()
+    if customerInfo:
+        if customerInfo.isActive is False:
+            context = {
+                'Status': 404,  # account is inactive
+            }
+            return render(request, 'login/index.html', context)
+        redirectUrl = reverse('portal-main')
+        response = HttpResponseRedirect(redirectUrl)
+        if rememberMe:
+            set_cookie(response, 'Session', '{}'.format(customerInfo.Session))
+        else:
+            set_cookie(response, 'Session', '{}'.format(customerInfo.Session), 5)  # 5 hour
+        return response
+    else:
+        context = {
+            'Status': 402,  # email or password is wrong
+        }
+        return render(request, 'login/index.html', context)
+
+
+def forgotPassword(request):
+    return render(request, 'login/forgotPassword.html')
